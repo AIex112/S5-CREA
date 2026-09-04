a9a4686d354754126368958f4a3ab924314d5692
AIex112 <alexandreguichon0@gmail.com>
2026-09-04 11:38:58 +0200

    4th

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-const name = "Hedy Lamarr";
-const age = 30;
-const monStyle = { backgroundColor: "lightgray", padding: "20px" };
+import Profile from "./components/Profile";
+
+function User({name, age}) {
+  return (
+    <div className="bg-amber-900 text-white p-4 m-4 rounded-lg shadow-md">
+      <h2>{name}</h2>
+      <p>Age: {age}</p>
+    </div>
+  );
+}
 
 function App() {
   return (
     <>
-      <h1 className="bg-amber-900 text-white p-4"> {name}'s Todos</h1>
-      <img src="https://i.imgur.com/yXOvdOSs.jpg" alt="Hedy Lamarr" />
-      <ul style={monStyle}>
-        <li>Invent new traffic lights {age +1 }</li>
-        <li>Rehearse a movie scene</li>
-        <li>Improve the spectrum technology</li>
-      </ul>
+      <User name="Hedy Lamarr" age={101} />
+      <User name="Ada Lovelace" age={207} />
+      <User name="Grace Hopper" age={155} />
     </>
   );
 }
