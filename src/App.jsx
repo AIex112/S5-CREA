3beca4b814d430d23138efc4d29dfa1186707954
AIex112 <alexandreguichon0@gmail.com>
2026-09-04 15:13:08 +0200

    7th commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import Profile from "./components/Profile";
 import Card from "./components/card";
 
 
@@ -13,13 +12,17 @@ function User({name, age}) {
 
 function App() {
   return (
-    <>
+    <div className="flex flex-col items-center justify-left min-h-screen bg-gray-100 p-4">
+    <section className="bg-gray-200 " flex items-center justify-center>
+      <h1 className="text-3xl font-bold pl-4 pt-4">Room</h1>
+      <p className="text-gray-600 mb-8 pl-4">5 people are around right now !</p>
       <Card name="Dale Houston" job="Software Engineer" image="https://testingbot.com/free-online-tools/random-avatar/50" />
       <Card name="Jane Doe" job="Product Manager" image="https://testingbot.com/free-online-tools/random-avatar/60" />
       <Card name="John Smith" job="Designer" image="https://testingbot.com/free-online-tools/random-avatar/70" />
       <Card name="Alice Johnson" job="Data Scientist" image="https://testingbot.com/free-online-tools/random-avatar/80" />
       <Card name="Bob Williams" job="Sales Associate" image="https://testingbot.com/free-online-tools/random-avatar/90" />
-    </>
+    </section>
+    </div>
   );
 }
 
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,12 +1,29 @@
-function Card( {name, job, image } ) {
-    return (
-        <>
-        <img className="w-16 h-16 rounded-full" src={image} alt={name} />
+function Card({ name, job, image }) {
+  return (
+    <div className="justify-left flex w-2xl items-center gap-4 bg-gray-200 p-4 text-black">
+      <img className="h-16 w-16 rounded-full" src={image} alt={name} />
+      <div className="flex w-60 flex-col">
         <h2>{name}</h2>
         <p>{job}</p>
-        <img src="https://i.imgur.com/yXOvdOSs.jpg" alt="tchat" />
-        </>
-    );
+      </div>
+      <button className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 ml-auto">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6。741v6。018Z"
+          />
+        </svg>
+      </button>
+    </div>
+  );
 }
 
 export default Card;
